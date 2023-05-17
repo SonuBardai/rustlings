086705fd22ea57af06349c9b9dcaf88b48dbc838
SonuBardai <67749330+SonuBardai@users.noreply.github.com>
2023-05-17 06:22:39 +0530

    Remove unused function

--- exercises/functions/functions1.rs
+++ exercises/functions/functions1.rs
@@ -1,9 +1,7 @@
 // functions1.rs
 // Execute `rustlings hint functions1` or use the `hint` watch subcommand for a hint.
 
-fn call_me() {
-    println!("You just called the best function in the whole wide world");
-}
+// I AM DONE
 
 fn main() {
     call_me();
